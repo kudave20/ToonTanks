d7ae527a11027bbac480d24a32d6415e615c3b40
DY <dave1315@korea.ac.kr>
2022-03-05 20:16:43 +0900

    발사 시 구체 생성

--- Source/ToonTanks/BasePawn.cpp
+++ Source/ToonTanks/BasePawn.cpp
@@ -4,6 +4,8 @@
 #include "BasePawn.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/StaticMeshComponent.h"
+#include "DrawDebugHelpers.h"
+
 
 // Sets default values
 ABasePawn::ABasePawn()
@@ -29,4 +31,18 @@ void ABasePawn::RotateTurret(FVector LookAtTarget)
 	FVector ToTarget = LookAtTarget - TurretMesh->GetComponentLocation();
 	FRotator LookAtRotation = FRotator(0.f, ToTarget.Rotation().Yaw, 0.f);
 	TurretMesh->SetWorldRotation(LookAtRotation);
+}
+
+void ABasePawn::Fire()
+{
+	FVector ProjectileSpawnPointLocation = ProjectileSpawnPoint->GetComponentLocation();
+	DrawDebugSphere(
+		GetWorld(), 
+		ProjectileSpawnPointLocation, 
+		25.f, 
+		12, 
+		FColor::Red, 
+		false, 
+		3.f
+	);
 }
